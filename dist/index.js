23506610389dd4a74a0ef70ac4a1f6c75a535408
Vinicius-de-Morais <vmorais194@gmail.com>
2023-11-01 21:21:13 -0300

    Adding cors

--- dist/index.js
+++ dist/index.js
@@ -7,12 +7,17 @@ import { Logger } from "tslog";
 import { AuthorizationChecker } from "./providers/authorization.js";
 import OpenaiController from "./controller/OpenaiController.js";
 import { RescueController } from "./controller/rescueController.js";
+import cors from 'cors';
 const logger = new Logger({ name: "main" });
 // creates express app, registers all controller routes and returns you express app instance
 const app = createExpressServer({
     controllers: [UserController, OpenaiController, RescueController],
-    authorizationChecker: AuthorizationChecker,
+    authorizationChecker: AuthorizationChecker
 });
+// Add the cors middleware to allow all origins
+app.use(cors({
+    origin: '*',
+}));
 // run express application on port 3000
 app.listen(process.env.PORT ?? 3000, () => {
     logger.info(`Servidor escutando na porta`, process.env.PORT ?? 3000);
--- src/index.ts
+++ src/index.ts
@@ -8,16 +8,23 @@ import { Logger } from "tslog";
 import type { Application } from "express";
 import { AuthorizationChecker } from "./providers/authorization.js";
 import OpenaiController from "./controller/OpenaiController.js";
-import path from "path";
 import { RescueController } from "./controller/rescueController.js";
+import path from "path";
+import cors from 'cors';
 
 const logger = new Logger({ name: "main" });
 
 // creates express app, registers all controller routes and returns you express app instance
 const app: Application = createExpressServer({
 	controllers: [UserController, OpenaiController, RescueController], // we specify controllers we want to use
-	authorizationChecker: AuthorizationChecker,
+	authorizationChecker: AuthorizationChecker
 });
+
+// Add the cors middleware to allow all origins
+app.use(cors({
+	origin: '*',
+  }));
+  
 
 // run express application on port 3000
 app.listen(process.env.PORT ?? 3000, () => {
